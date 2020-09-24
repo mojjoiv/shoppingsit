11d8cc1def415ad4d505087e31cf93270def1ac5
Dalton <patrick.ongeche@mawingunetworks.com>
2020-09-24 15:17:16 +0300

    db change

--- includes/config.php
+++ includes/config.php
@@ -7,8 +7,8 @@
 
 //remote db connection
 define('DB_SERVER','remotemysql.com');
-define('DB_USER','tWpRlUo0aa');
-define('DB_PASS' ,'SSfpgWOtNF');
+define('DB_USER','6nQO1o7m4f');
+define('DB_PASS' ,'oQyLE2mg1e');
 define('DB_NAME', 'tWpRlUo0aa');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
