9a8e5b9a50c7ee30f612f183603057f6e5c93078
Dalton <patrick.ongeche@mawingunetworks.com>
2020-09-24 13:48:24 +0300

    remote db

--- includes/config.php
+++ includes/config.php
@@ -1,15 +1,15 @@
 <?php
 //development server
-define('DB_SERVER','localhost');
-define('DB_USER','root');
-define('DB_PASS' ,'');
-define('DB_NAME', 'shopping');
+// define('DB_SERVER','localhost');
+// define('DB_USER','root');
+// define('DB_PASS' ,'');
+// define('DB_NAME', 'shopping');
 
 //remote db connection
-// define('DB_SERVER','remotemysql.com');
-// define('DB_USER','tWpRlUo0aa');
-// define('DB_PASS' ,'SSfpgWOtNF');
-// define('DB_NAME', 'tWpRlUo0aa');
+define('DB_SERVER','remotemysql.com');
+define('DB_USER','tWpRlUo0aa');
+define('DB_PASS' ,'SSfpgWOtNF');
+define('DB_NAME', 'tWpRlUo0aa');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
 if (mysqli_connect_errno())
