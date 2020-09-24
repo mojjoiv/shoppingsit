4aee0b22ee35ed8aab19391eaed50670192a702b
Dalton <patrick.ongeche@mawingunetworks.com>
2020-09-24 12:34:40 +0300

    first commit

---
+++ includes/config.php
@@ -0,0 +1,19 @@
+<?php
+//development server
+define('DB_SERVER','localhost');
+define('DB_USER','root');
+define('DB_PASS' ,'');
+define('DB_NAME', 'shopping');
+
+//remote db connection
+// define('DB_SERVER','remotemysql.com');
+// define('DB_USER','tWpRlUo0aa');
+// define('DB_PASS' ,'SSfpgWOtNF');
+// define('DB_NAME', 'tWpRlUo0aa');
+$con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
+// Check connection
+if (mysqli_connect_errno())
+{
+ echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+?>
