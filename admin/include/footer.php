e1d98b46819f5b7ffd9557710c8baaf8e35cadb9
Dalton <patrick.ongeche@mawingunetworks.com>
2020-09-24 18:30:28 +0300

    footer change

--- admin/include/footer.php
+++ admin/include/footer.php
@@ -2,6 +2,10 @@
 		<div class="container">
 			 
 
-			<b class="copyright">&copy; 2017 Shopping Portal </b> All rights reserved.
+		Copyright &copy;<script>
+						document.write(new Date().getFullYear());
+					</script> 2020 SheaNayomi | All rights reserved <i class="fa fa-heart-o"
+						aria-hidden="true"></i> <br>
+						Powered by <a href="https://autifyy.netlify.app/" target="_blank">AUTIFY</a>
 		</div>
 	</div>
